f8f332138ae0fce57c9eed41e71ae26713d4ced2
tudusbagus <joaopedromassonbarnabe@gmail.com>
2025-04-16 15:35:28 -0300

    Add files via upload

---
+++ lab04.py
@@ -0,0 +1,16 @@
+Vj = int(input())
+Ve = int(input())
+Dj = int(input())
+De = int(input())
+Rj = int(input())
+Re = int(input())
+Cj = int(input())
+Ce = int(input())
+
+Jinx =  0
+Ekko = 1
+
+while(Jinx):
+    print(Ve - Dj)
+    if (Ve - Dj >= 0):
+        print(Jinx)
